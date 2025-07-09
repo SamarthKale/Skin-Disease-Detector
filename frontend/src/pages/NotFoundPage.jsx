4809dadaa5929c23e4854cf3238f44bb75ca440d
Abhinaya Gowda <gowdaabhinaya9@gmail.com>
2025-07-09 22:19:58 +0530

    Create NotFoundPage.jsx

---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div style={{ padding: "3rem", textAlign: "center" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
